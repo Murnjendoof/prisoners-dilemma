8b01594d89e13086d38cee6e6b80e3f73cd7b014
Murnjendoof <55724715+Murnjendoof@users.noreply.github.com>
2021-05-21 12:27:40 +1200

    Add files via upload

---
+++ prisoner'sDilemma/theGame.java
@@ -0,0 +1,41 @@
+
+/**
+ * The prisoner's dilemma
+ *
+ * @author Solomon Emet
+ * @version 13/5/2021
+ */
+public class theGame
+{
+
+    /**
+     * Constructor for objects of class theGame
+     */
+    public theGame()
+    {
+        System.out.println("You've been arrested for tax evasion.");
+        System.out.println("Fortunately for you, the cops only have enough evidence to put you away for littering.");
+        System.out.println("Even so, it's enough to put you away for two years.");
+        System.out.println("The cops are interrogating you and your accomplice seperately.");
+        System.out.println("If you both keep quiet, you'll both get the short sentence.");
+        System.out.println("If you both snitch on each other, you'll both get a huge honkin' sentence.");
+        System.out.println("But if you snitch, and your accomplice keeps quiet, the cops will let you go free while giving your buddy life.");
+        System.out.println("And if you keep quiet, but your accomplice snitches, they'll go free and you'll get life.");
+        System.out.println("This is the prisoner's dilemma.");
+        while(true){
+            System.out.println("How many rounds do you want to play?");
+        }
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return y;
+    }
+}
